cd17697c81d13eca8b5b9fc0d18f1f0e10747d34
CFink01 <92890248+CFink01@users.noreply.github.com>
2022-02-24 10:14:56 -0500

    Add files via upload

--- DirControl.py
+++ DirControl.py
@@ -1,21 +1,31 @@
+import sys
 def FileSystem():
     import os 
+    from PrintColors import PrintColor
     from os.path import expanduser
+    import sys
+
+    if sys.platform != 'win32':
+        doccumentsDir = '/var'
+        
 
     #Find user home directory
     homeDir = expanduser("~")
 
-    #Join home directory path to desktop
-    desktop = os.path.join(homeDir, "Desktop\\")
+    #Join home directory path to Doccuments
+    Doccuments = os.path.join(homeDir, "Doccuments\\")
 
     #IF the ToDo List directory doesn't exist
-    if os.path.isdir(desktop + "ToDo List\\") == False:
-
+    if os.path.isdir(Doccuments + "ToDo List\\") == False:
+        os.mkdir(Doccuments + "ToDo List\\")
+        print("NOTICE:")
+        PrintColor.prRed("directory Created")
         #Make ToDo List directory
-        os.mkdir(desktop + "ToDo List\\")
 
     #Make variable with ToDo List directory
-    TodoDir = os.path.join(desktop, "ToDo List\\")
-    
+    TodoDir = os.path.join(Doccuments, "ToDo List\\")
+
     #Return ToDo List directory
     return TodoDir
+
+print(sys.platform)
